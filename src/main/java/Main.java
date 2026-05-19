239a28550f026690625dea90adc817ed08991a32
Sakshi <sakshi_jain75@gmail.com>
2026-05-20 00:54:12 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,9 +21,29 @@ public class Main {
                 String val = cmd.substring(5).trim();
                 if(builtin.contains(val)) {
                     System.out.println(val+" is a shell builtin");
-                }
-                else {
-                    System.out.println(val + ": not found");
+                    continue;
+                } else {
+                    Path path = Paths.get("");
+                    String currentPath = path.toAbsolutePath().toString();
+                    String sep = path.getFileSystem().getSeparator();
+                    String[] dirs = currentPath.split("\\"+sep);
+                    StringBuilder p = new StringBuilder();
+                    Boolean flag = false;
+                    for(String dir: dirs) {
+                        if(!p.isEmpty())
+                            p.append("/").append(dir);
+                        else
+                            p.append(dir);
+                        String temp = p +"/"+val;
+                        File file = new File(temp);
+                        if (file.canExecute()) {
+                           System.out.println(val+" is "+file.getPath());
+                           flag = true;
+                           break;
+                        }
+                    }
+                    if(!flag)
+                        System.out.println(val + ": not found");
                 }
             } else
                 System.out.println(cmd + ": command not found");
