f3ba3884a396c72fbdc9f6f4b709e1ec917cabe0
Sakshi <sakshi_jain75@gmail.com>
2026-05-20 01:25:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+
+    static String executablePath(String val) {
+        String[] dirs = System.getenv("PATH").split(File.pathSeparator);
+        for(String dir: dirs) {
+            String temp = dir+File.separator+val;
+            System.out.println(temp);
+            File file = new File(temp);
+            if (file.canExecute()) {
+                return (file.getPath());
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
@@ -15,28 +27,28 @@ public class Main {
             String cmd = scanner.nextLine();
             if("exit".equals(cmd))
                 break;
-            else if (cmd.startsWith("echo ")) {
+            if (cmd.startsWith("echo ")) {
                 System.out.println(cmd.replace("echo ", ""));
             } else if (cmd.startsWith("type ")) {
                 String val = cmd.substring(5).trim();
                 if(builtin.contains(val)) {
                     System.out.println(val+" is a shell builtin");
                 } else {
-                    String[] dirs = System.getenv("PATH").split(File.pathSeparator);
-                    boolean flag = false;
-                    for(String dir: dirs) {
-                        String temp = dir+File.separator+val;
-                        File file = new File(temp);
-                        if (file.canExecute()) {
-                           System.out.println(val+" is "+file.getPath());
-                           flag = true;
-                           break;
-                        }
-                    }
-                    if(!flag)
+                    String result = executablePath(val);
+                    if(result!=null)
+                        System.out.println(val+" is "+result);
+                    else
                         System.out.println(val + ": not found");
+                    continue;
                 }
-            } else
+            } else {
+                String[] input = cmd.split(" ");
+                if(executablePath(input[0])!=null) {
+                    ProcessBuilder processBuilder = new ProcessBuilder(executablePath(input[0]));
+                    processBuilder.start();
+
+                }
+            }
                 System.out.println(cmd + ": command not found");
         }
     }
