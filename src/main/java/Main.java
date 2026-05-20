3699cecbe6c154485625bfe0e1b52d5f35085a4d
Sakshi <sakshi_jain75@gmail.com>
2026-05-20 20:35:32 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,60 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    static String executablePath(String val) {
+    static List<String> builtin = List.of("echo", "exit", "type", "pwd", "cd");
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print("$ ");
+            String cmd = scanner.nextLine();
+            if("exit".equals(cmd))
+                break;
+            if (cmd.startsWith("echo ")) {
+                System.out.println(cmd.replace("echo ", ""));
+            } else if (cmd.startsWith("type ")) {
+                executeTypeCommand(cmd);
+            } else if ("pwd".equals(cmd)) {
+                System.out.println(Paths.get("").toAbsolutePath());
+            } else if (cmd.startsWith("cd ")) {
+                String newPath = cmd.split(" ")[1];
+                System.out.println(newPath);
+                if(Files.exists(Path.of(newPath))) {
+                    System.out.println("Path exists");
+                }
+                else
+                    System.out.println("cd: "+newPath+": No such file or directory");
+            } else {
+                String[] input = cmd.split(" ");
+                if(executablePath(input[0])!=null)
+                    executeCustomCommand(input);
+                else
+                    System.out.println(cmd + ": command not found");
+            }
+        }
+    }
+
+    private static void executeTypeCommand(String cmd) {
+        String val = cmd.substring(5).trim();
+        if(builtin.contains(val)) {
+            System.out.println(val+" is a shell builtin");
+        } else {
+            String result = executablePath(val);
+            if(result!=null)
+                System.out.println(val+" is "+result);
+            else
+                System.out.println(val + ": not found");
+        }
+    }
+
+    private static String executablePath(String val) {
         String[] dirs = System.getenv("PATH").split(File.pathSeparator);
         for(String dir: dirs) {
             String temp = dir+File.separator+val;
@@ -19,41 +66,10 @@ public class Main {
         return null;
     }
 
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        Scanner scanner = new Scanner(System.in);
-        List<String> builtin = List.of("echo", "exit", "type", "pwd");
-        while(true) {
-            System.out.print("$ ");
-            String cmd = scanner.nextLine();
-            if("exit".equals(cmd))
-                break;
-            if (cmd.startsWith("echo ")) {
-                System.out.println(cmd.replace("echo ", ""));
-            } else if (cmd.startsWith("type ")) {
-                String val = cmd.substring(5).trim();
-                if(builtin.contains(val)) {
-                    System.out.println(val+" is a shell builtin");
-                } else {
-                    String result = executablePath(val);
-                    if(result!=null)
-                        System.out.println(val+" is "+result);
-                    else
-                        System.out.println(val + ": not found");
-                }
-            } else if (cmd.equals("pwd")) {
-                System.out.println(Paths.get("").toAbsolutePath());
-            } else {
-                String[] input = cmd.split(" ");
-                if(executablePath(input[0])!=null) {
-                    ProcessBuilder processBuilder = new ProcessBuilder(input);
-                    Process process = processBuilder.start();
-                    process.getInputStream().transferTo(System.out);
-                    process.waitFor();
-                }
-                else
-                    System.out.println(cmd + ": command not found");
-            }
-        }
+    private static void executeCustomCommand(String[] input) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder(input);
+        Process process = processBuilder.start();
+        process.getInputStream().transferTo(System.out);
+        process.waitFor();
     }
 }
